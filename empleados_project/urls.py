0d7580f6d7e3b9cd7e895a824b2c25477ade8951
Jesús Perafán <jesusperafan1995@gmail.com>
2026-09-15 15:05:13 -0500

    Agrega redireccionamiento desde la ruta principal

--- empleados_project/urls.py
+++ empleados_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+
+    path('', lambda request: redirect('empleados')),
+    
     path('admin/', admin.site.urls),
 
     path('empleados/', include('empleados.urls')),
